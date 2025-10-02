3ffa893f8b1551066a3550a350fa5650a7ff051a
0x00000000 <kden9382@gmail.com>
2025-10-02 17:30:26 +0300

    v1.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static final Scanner scanner = new Scanner(System.in);
-    private static final String FILEPATH = "logs/logs.txt";
+    private static final String FILEPATH = "logs.txt";
     private static final LogFileReader reader = new LogFileReader();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         reader.readLogFile(FILEPATH);
         Menu menu = new Menu(scanner, reader.getLogRecords(), reader.getLogInvalidRecords(), FILEPATH);
         menu.start();
